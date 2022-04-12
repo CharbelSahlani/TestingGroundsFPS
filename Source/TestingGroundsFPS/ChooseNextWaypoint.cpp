6d0fe7439bda9caee294cde8509ed5f64bb588c0
CSahlani <charbel.alsahlani@gmail.com>
2022-04-12 19:48:32 +0300

    added the ChooseNextWaypoint.cpp

---
+++ Source/TestingGroundsFPS/ChooseNextWaypoint.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ChooseNextWaypoint.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	auto BlackBoardComp = OwnerComp.GetBlackboardComponent();
+	auto Index = BlackBoardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+	return  EBTNodeResult::Succeeded;
+}
