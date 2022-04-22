834737b86532fd84a663fa2add7d9c50f4249816
CSahlani <charbel.alsahlani@gmail.com>
2022-04-22 10:26:15 +0300

    cycling through patrol points in cpp

--- Source/TestingGroundsFPS/ChooseNextWaypoint.cpp
+++ Source/TestingGroundsFPS/ChooseNextWaypoint.cpp
@@ -2,11 +2,28 @@
 
 
 #include "ChooseNextWaypoint.h"
+#include "AIController.h"
+#include "PatrollingGuard.h" //TODO remove coupling
 #include "BehaviorTree/BlackboardComponent.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
+	//TODO Protect against empty patrol routes
+	
+
+	//Get the patrol points
+	auto AIController = OwnerComp.GetAIOwner();
+	auto ControlledPawn = AIController->GetPawn();
+	auto PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
+	auto PatrolPoints = PatrollingGuard->PatrolPointsCPP;
+
+	//Set next waypoint
 	auto BlackBoardComp = OwnerComp.GetBlackboardComponent();
 	auto Index = BlackBoardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+	BlackBoardComp->SetValueAsObject(WaypointKey.SelectedKeyName, PatrolPoints[Index]);
+
+	//Cycle the index
+	auto NextIndex = (Index + 1) % PatrolPoints.Num();
+	BlackBoardComp->SetValueAsInt(IndexKey.SelectedKeyName, NextIndex);
 	return  EBTNodeResult::Succeeded;
 }
